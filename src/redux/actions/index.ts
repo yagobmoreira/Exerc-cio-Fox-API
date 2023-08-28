0a0b3a07b97e1817fe13c2735a15df26cfbbbebc
yagobmoreira <131387659+yagobmoreira@users.noreply.github.com>
2023-08-28 11:08:30 -0300

    2 files modified

--- src/redux/actions/index.ts
+++ src/redux/actions/index.ts
@@ -1,3 +1,5 @@
+import { Dispatch } from '../../types';
+
 export const REQUEST_API = 'REQUEST_API';
 export const GET_PICTURE = 'GET_PICTURE';
 
@@ -9,5 +11,14 @@ export const getPicture = (image: string) => ({
 });
 
 export function fetchAPI() {
-  // Criar
+  return async (dispatch: Dispatch) => {
+    try {
+      dispatch(requestAPI);
+      const response = await fetch('https://randomfox.ca/floof/');
+      const data = await response.json();
+      dispatch(getPicture(data.image));
+    } catch (error: any) {
+      console.log(error.message);
+    }
+  };
 }
